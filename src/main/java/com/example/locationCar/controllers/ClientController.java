ba9f2cbbc9bc39aa92b796ea731909aae9cf4baa
Anna Carolina Hermes <anna.hermes@gmail.com>
2023-07-28 14:56:31 -0300

    atualizando controller

--- src/main/java/com/example/locationCar/controllers/ClientController.java
+++ src/main/java/com/example/locationCar/controllers/ClientController.java
@@ -1,12 +1,9 @@
 package com.example.locationCar.controllers;
 
-import com.example.locationCar.repositories.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
-    @Autowired
-    ClientRepository clientRepository;
+
 
 }
