6c39c57cce44ad5d7a7a52b5e1b27ee94ebbdc89
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-07-29 08:40:48 -0300

    edita Controller e cria deleteFuncionario - controller. Endpoint /{id}

--- src/main/java/com/example/locationCar/controller/FuncionarioController.java
+++ src/main/java/com/example/locationCar/controller/FuncionarioController.java
@@ -1,16 +1,36 @@
 package com.example.locationCar.controller;
 
 
+import com.example.locationCar.models.FuncionarioModel;
 import com.example.locationCar.repositories.FuncionarioRepository;
+import com.example.locationCar.services.funcionarioService.FuncionarioServiceDelete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/funcionario")
+@RequestMapping("/v1/funcionario")
 public class FuncionarioController {
 
-    @Autowired
     FuncionarioRepository funcionarioRepository;
+    FuncionarioServiceDelete funcionarioServiceDelete;
 
+    public FuncionarioController(FuncionarioServiceDelete funcionarioServiceDelete) {
+        this.funcionarioServiceDelete = funcionarioServiceDelete;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteFuncionario(@PathVariable(value = "id") UUID funcionarioId) {
+        FuncionarioModel funcionario = funcionarioServiceDelete.deleteFuncionario(funcionarioId);
+        return (funcionario == null || !funcionario.getIdFuncionario().equals(funcionarioId)) ? ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Employee not found")
+                : ResponseEntity.status(HttpStatus.OK).body("Employee deleted successfully");
+    }
 }
