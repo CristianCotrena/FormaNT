73053c2c6930d0d78eaf85156ef3b14916826ad4
BrunoRRamos88 <137199296+BrunoRRamos88@users.noreply.github.com>
2023-07-31 15:23:40 -0300

    Atualização do código e para pagoigo para inglês

--- src/main/java/controllers/AddressController.java
+++ src/main/java/controllers/AddressController.java
@@ -3,6 +3,6 @@ package controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EnderecoController {
+public class AddressController {
 
 }
---
+++ src/main/java/dtos/AddressRecordDto.java
@@ -0,0 +1,9 @@
+package dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AddressRecordDto(@NotBlank String publicPlace, @NotNull int number,
+                               @NotBlank String complement, @NotBlank String city, @NotBlank String state,
+                               @NotBlank String country, @NotBlank String cep, @NotNull int status) {
+}
