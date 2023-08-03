ce9de416225a8167b61dcca15a1baefabf16e178
Guilherme Lemos <109246414+guilhermelemosp@users.noreply.github.com>
2023-08-03 15:54:34 -0300

    finaliza documentação de DeleteEmployee

--- src/main/java/com/example/locationCar/controllers/EmployeeController.java
+++ src/main/java/com/example/locationCar/controllers/EmployeeController.java
@@ -8,15 +8,21 @@ import com.example.locationCar.services.funcionarioService.EmployeeService;
 import com.example.locationCar.services.funcionarioService.EmployeeServiceDelete;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/employee")
+@Tag(name = "Employee", description = "Operations about Employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -36,6 +42,13 @@ public class EmployeeController {
         return  ResponseEntity.status(HttpStatus.CREATED).body(employeeModel );
     }
 
+    @Operation(summary = "Delete Employee", description = "Delete an employee to database")
+    @ApiResponse(responseCode = "200", description = "OK", content = {
+            @Content(mediaType = "text/plain", schema = @Schema(type = "string", example = "Employee deleted successfully"))
+    })
+    @ApiResponse(responseCode = "404", description = "Employee not found", content = {
+            @Content(mediaType = "text/plain", schema = @Schema(type = "string", example = "Employee not found")),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteEmployee(@PathVariable(value = "id") UUID employeeId) {
         EmployeeModel employee = employeeServiceDelete.deleteEmployee(employeeId);
