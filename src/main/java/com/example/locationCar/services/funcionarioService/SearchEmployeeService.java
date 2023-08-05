73c3eaf2fcbee9b322cd69361ec14ca76a9324a5
Anna Carolina Hermes <anna.hermes@gmail.com>
2023-08-05 20:59:11 -0300

    criação da service de busca de funcionário, implementação de testes e atualização do collection postman

---
+++ src/main/java/com/example/locationCar/services/funcionarioService/SearchEmployeeService.java
@@ -0,0 +1,47 @@
+package com.example.locationCar.services.funcionarioService;
+
+import com.example.locationCar.models.EmployeeModel;
+import com.example.locationCar.repositories.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.locationCar.services.funcionarioService.EmployeeService.isValidEmail;
+
+@Service
+public class SearchEmployeeService {
+
+    EmployeeRepository employeeRepository;
+
+    public SearchEmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public EmployeeModel employeeSearchById(UUID id) {
+        Optional<EmployeeModel> optionalEmployee = employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()) {
+            return optionalEmployee.get();
+        } else {
+            throw new IllegalArgumentException("Funcionário não encontrado pelo ID informado.");
+        }
+    }
+
+    public EmployeeModel employeeSearchByCpfCnpj(String cpfCnpj) {
+        EmployeeModel employeeModel = employeeRepository.findByCpfCnpj(cpfCnpj);
+        if (employeeModel != null) {
+            return employeeModel;
+        } else {
+            throw new IllegalArgumentException("Funcionário não encontrado pelo CPF/CNPJ informado.");
+        }
+    }
+
+    public EmployeeModel employeeSearchByEmail(String email) {
+            EmployeeModel employeeModel = employeeRepository.findByEmail(email);
+            if (employeeModel != null) {
+                return employeeModel;
+            } else {
+                throw new IllegalArgumentException("Funcionário não encontrado pelo e-mail informado.");
+            }
+    }
+}
---
+++ src/main/java/com/example/locationCar/services/funcionarioService/test/SearchEmployeeTest.java
@@ -0,0 +1,82 @@
+package com.example.locationCar.services.funcionarioService.test;
+
+import com.example.locationCar.models.EmployeeModel;
+import com.example.locationCar.models.enums.ContractType;
+import com.example.locationCar.models.enums.Position;
+import com.example.locationCar.models.enums.Role;
+import com.example.locationCar.repositories.EmployeeRepository;
+import com.example.locationCar.services.funcionarioService.SearchEmployeeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchEmployeeTest {
+
+    @InjectMocks
+    private SearchEmployeeService searchEmployeeService;
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    private EmployeeModel employeeModel;
+
+    @BeforeEach
+    void setup() {
+        employeeModel = new EmployeeModel();
+        employeeModel.setEmployeeId(UUID.randomUUID());
+        employeeModel.setName("Joana");
+        employeeModel.setPosition(Position.VENDEDOR);
+        employeeModel.setCpfCnpj("54506305302");
+        employeeModel.setRegistry("1234");
+        employeeModel.setPhone("5133340436");
+        employeeModel.setContractType(ContractType.CLT);
+        employeeModel.setRole(Role.VENDEDOR);
+        employeeModel.setEmail("joana@gmail.com");
+        employeeModel.setStatus(1);
+    }
+
+    @Test
+    public void testGetEmployeeById() {
+        UUID id = employeeModel.getEmployeeId();
+
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(employeeModel));
+
+        EmployeeModel result = searchEmployeeService.employeeSearchById(id);
+
+        assertNotNull(result);
+        assertEquals(employeeModel, result);
+    }
+
+    @Test
+    public void testGetEmployeeByCpfCnpj() {
+        String cpfCnpj = employeeModel.getCpfCnpj();
+
+        when(employeeRepository.findByCpfCnpj(cpfCnpj)).thenReturn(employeeModel);
+
+        EmployeeModel result = searchEmployeeService.employeeSearchByCpfCnpj(cpfCnpj);
+
+        assertNotNull(result);
+        assertEquals(employeeModel, result);
+    }
+
+    @Test
+    public void testGetEmployeeByEmail() {
+        String email = employeeModel.getEmail();
+
+        when(employeeRepository.findByEmail(email)).thenReturn(employeeModel);
+
+        EmployeeModel result = searchEmployeeService.employeeSearchByEmail(email);
+
+        assertNotNull(result);
+        assertEquals(employeeModel, result);
+    }
+}
