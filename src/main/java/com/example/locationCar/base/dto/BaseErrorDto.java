efb7ad9a7cb3319eb392f42c21c92d25a8a1d57f
vini <viniciusdemoraisrosa@gmail.com>
2023-08-15 19:08:05 -0300

    feat(estrutura)#59: padronizando retorno

---
+++ src/main/java/com/example/locationCar/base/dto/BaseErrorDto.java
@@ -0,0 +1,27 @@
+package com.example.locationCar.base.dto;
+
+public class BaseErrorDto {
+    private String field;
+    private String message;
+
+    public BaseErrorDto(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
