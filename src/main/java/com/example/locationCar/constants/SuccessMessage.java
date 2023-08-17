fee3c63db911ef07df7ac77f9a11c34637f22736
Gustavo <gustavoneri20@hotmail.com>
2023-08-17 20:46:26 -0300

    fix(funcionario)#27: alteração para novo retorno e melhoria dos erros

--- src/main/java/com/example/locationCar/constants/SuccessMessage.java
+++ src/main/java/com/example/locationCar/constants/SuccessMessage.java
@@ -2,4 +2,5 @@ package com.example.locationCar.constants;
 
 public @interface SuccessMessage {
     String CREATE_CLIENT = "Cliente criado com sucesso";
+    String LIST_EMPLOYEES = "Funcionários listados com sucesso";
 }
