2b2839d4c09f06f6a4d07df81e82003a2009c28e
vini <viniciusdemoraisrosa@gmail.com>
2023-09-12 16:25:33 -0300

    doc-and-postman(criar-endereco)#36: criacao da documentação e do postman para criacao de endereco

--- src/main/java/com/example/locationCar/controllers/AddressController.java
+++ src/main/java/com/example/locationCar/controllers/AddressController.java
@@ -3,6 +3,8 @@ package com.example.locationCar.controllers;
 import com.example.locationCar.base.dto.BaseDto;
 import com.example.locationCar.dtos.input.AddressInputDto;
 import com.example.locationCar.services.addressService.CreateAddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +20,9 @@ public class AddressController {
     public AddressController(CreateAddressService createAddressService) {
         this.createAddressService = createAddressService;
     }
-
+    @Operation(summary = "Create address", description = "Add an address to the database")
+    @ApiResponse(responseCode = "201", description = "Created")
+    @ApiResponse(responseCode = "400", description = "Invalid data")
     @PostMapping
     public ResponseEntity<BaseDto> createAddress(@RequestBody AddressInputDto addressInputDto){
         BaseDto baseDto = createAddressService.inserir(addressInputDto);
