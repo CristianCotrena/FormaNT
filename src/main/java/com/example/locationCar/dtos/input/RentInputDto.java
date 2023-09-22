ec93d80c898dbb256751b52e4bff931b57da52f6
vini <viniciusdemoraisrosa@gmail.com>
2023-09-21 23:41:30 -0300

    task/46: criacao de postman e doc para criar aluguel

--- src/main/java/com/example/locationCar/dtos/input/RentInputDto.java
+++ src/main/java/com/example/locationCar/dtos/input/RentInputDto.java
@@ -1,17 +1,25 @@
 package com.example.locationCar.dtos.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
 
 public class RentInputDto {
-
+    @Schema(example = "f330b8fb-4e38-44f5-a096-11fbc43d07d3")
     private UUID idClient;
+    @Schema(example = "f330b8fb-4e38-44f5-a096-11fbc43d07d3")
     private UUID idEmployee;
+    @Schema(example = "f330b8fb-4e38-44f5-a096-11fbc43d07d3")
     private UUID idVehicle;
+    @Schema(example = "1")
     private Integer haveInsurance;
+    @Schema(example = "1")
     private Integer status;
+    @Schema(example = "2045-03-11T00:00:00+02:00")
     private ZonedDateTime contractingDate;
+    @Schema(example = "2045-05-09T00:00:00+02:00")
     private ZonedDateTime returnDate;
 
     public RentInputDto() {
