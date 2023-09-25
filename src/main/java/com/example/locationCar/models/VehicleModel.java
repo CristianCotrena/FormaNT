7a7ef488a7866cc5dae80c2ea3c0b606a54c9691
Anna Carolina Hermes <anna.hermes@gmail.com>
2023-09-25 18:14:21 -0300

    fix(estrutura)#68: resolvendo conflitos

--- src/main/java/com/example/locationCar/models/VehicleModel.java
+++ src/main/java/com/example/locationCar/models/VehicleModel.java
@@ -2,12 +2,19 @@ package com.example.locationCar.models;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
 @Table(name = "TB_VEHICLE")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleModel implements Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -45,119 +52,4 @@ public class VehicleModel implements Serializable {
 
   @Schema(example = "5.00")
   private BigDecimal rating;
-
-  public VehicleModel() {}
-
-  public VehicleModel(
-      UUID idVehicle,
-      String license,
-      String brand,
-      String model,
-      int doorNumber,
-      String color,
-      String fuel,
-      BigDecimal dailyValue,
-      BigDecimal mileage,
-      int status,
-      BigDecimal rating) {
-    this.idVehicle = idVehicle;
-    this.license = license;
-    this.brand = brand;
-    this.model = model;
-    this.doorNumber = doorNumber;
-    this.color = color;
-    this.fuel = fuel;
-    this.dailyValue = dailyValue;
-    this.mileage = mileage;
-    this.status = status;
-    this.rating = rating;
-  }
-
-  public UUID getIdVehicle() {
-    return idVehicle;
-  }
-
-  public void setIdVehicle(UUID idVehicle) {
-    this.idVehicle = idVehicle;
-  }
-
-  public String getLicense() {
-    return license;
-  }
-
-  public void setLicense(String license) {
-    this.license = license;
-  }
-
-  public String getBrand() {
-    return brand;
-  }
-
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
-
-  public String getModel() {
-    return model;
-  }
-
-  public void setModel(String model) {
-    this.model = model;
-  }
-
-  public Integer getDoorNumber() {
-    return doorNumber;
-  }
-
-  public void setDoorNumber(Integer doorNumber) {
-    this.doorNumber = doorNumber;
-  }
-
-  public String getColor() {
-    return color;
-  }
-
-  public void setColor(String color) {
-    this.color = color;
-  }
-
-  public String getFuel() {
-    return fuel;
-  }
-
-  public void setFuel(String fuel) {
-    this.fuel = fuel;
-  }
-
-  public BigDecimal getDailyValue() {
-    return dailyValue;
-  }
-
-  public void setDailyValue(BigDecimal dailyValue) {
-    this.dailyValue = dailyValue;
-  }
-
-  public BigDecimal getMileage() {
-    return mileage;
-  }
-
-  public void setMileage(BigDecimal mileage) {
-    this.mileage = mileage;
-  }
-
-  public Integer getStatus() {
-    return status;
-  }
-
-  public void setStatus(Integer status) {
-    this.status = status;
-  }
-
-  public BigDecimal getRating() {
-    return rating;
-  }
-
-  public void setRating(BigDecimal rating) {
-    this.rating = rating;
-  }
 }
