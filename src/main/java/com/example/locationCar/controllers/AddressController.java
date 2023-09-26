289756de15d8bc4fbe46ba67cea195e07a6ea44e
Franciele Santos <francielesantosadm@gmail.com>
2023-09-26 14:04:00 -0300

    feat(endereco)#39: atualização de endereço

--- src/main/java/com/example/locationCar/controllers/AddressController.java
+++ src/main/java/com/example/locationCar/controllers/AddressController.java
@@ -1,24 +1,33 @@
 package com.example.locationCar.controllers;
 
 import com.example.locationCar.base.dto.BaseDto;
+import com.example.locationCar.base.dto.BaseErrorDto;
+import com.example.locationCar.builder.ResponseErrorBuilder;
+import com.example.locationCar.constants.ErrorMessage;
+import com.example.locationCar.dtos.AddressUpdateDto;
 import com.example.locationCar.dtos.input.AddressInputDto;
 import com.example.locationCar.services.addressService.CreateAddressService;
+import com.example.locationCar.services.addressService.UpdateAddressService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/address")
 public class AddressController {
 
     private CreateAddressService createAddressService;
+    private UpdateAddressService updateAddressService;
 
-    public AddressController(CreateAddressService createAddressService) {
+    public AddressController(CreateAddressService createAddressService, UpdateAddressService updateAddressService) {
         this.createAddressService = createAddressService;
+        this.updateAddressService = updateAddressService;
     }
     @Operation(summary = "Create address", description = "Add an address to the database")
     @ApiResponse(responseCode = "201", description = "Created")
@@ -29,5 +38,18 @@ public class AddressController {
         return ResponseEntity.status(baseDto.getResult().getStatusCode()).body(baseDto);
     }
 
+    @PutMapping("/{id}")
+    public BaseDto<Void> updateAddress(@PathVariable String id, @RequestBody AddressUpdateDto addressUpdateDto) {
+        List<BaseErrorDto> errors = new ArrayList<>();
+        try{
+            UUID idAddress = UUID.fromString(id);
+            BaseDto updatedAddressId = updateAddressService.updateAddress(idAddress, addressUpdateDto);
+            return updatedAddressId;
+        }catch (IllegalArgumentException e){
+            errors.add(new BaseErrorDto ("idAddress", ErrorMessage.INVALID_FIELD));
+            ResponseErrorBuilder result = new ResponseErrorBuilder(HttpStatus.PRECONDITION_FAILED, errors);
+            return result.get();
+        }
+    }
 }
 
