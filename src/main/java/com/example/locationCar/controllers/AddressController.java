45e7a7536073e4ab37f649c458027437edafaa57
Franciele Santos <francielesantosadm@gmail.com>
2023-10-02 16:49:01 -0300

    task(endereco)#39:implementação da doc swagger e postman collection

--- src/main/java/com/example/locationCar/controllers/AddressController.java
+++ src/main/java/com/example/locationCar/controllers/AddressController.java
@@ -9,6 +9,8 @@ import com.example.locationCar.dtos.input.AddressInputDto;
 import com.example.locationCar.services.addressService.CreateAddressService;
 import com.example.locationCar.services.addressService.UpdateAddressService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,24 +31,48 @@ public class AddressController {
         this.createAddressService = createAddressService;
         this.updateAddressService = updateAddressService;
     }
+
     @Operation(summary = "Create address", description = "Add an address to the database")
     @ApiResponse(responseCode = "201", description = "Created")
     @ApiResponse(responseCode = "400", description = "Invalid data")
     @PostMapping
-    public ResponseEntity<BaseDto> createAddress(@RequestBody AddressInputDto addressInputDto){
+    public ResponseEntity<BaseDto> createAddress(@RequestBody AddressInputDto addressInputDto) {
         BaseDto baseDto = createAddressService.inserir(addressInputDto);
         return ResponseEntity.status(baseDto.getResult().getStatusCode()).body(baseDto);
     }
 
+    @Operation(summary = "Update address", description = "Update address")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Updated",
+            content = {
+                    @Content(mediaType = "text/plain", schema = @Schema(type = "string", format = "uuid"))
+            })
+    @ApiResponse(
+            responseCode = "404",
+            description = "Not found",
+            content = {
+                    @Content(
+                            mediaType = "text/plain",
+                            schema = @Schema(type = "string", example = "Não encontrado"))
+            })
+    @ApiResponse(
+            responseCode = "412",
+            description = "Invalid data",
+            content = {
+                    @Content(
+                            mediaType = "text/plain",
+                            schema = @Schema(type = "string", example = "Campo inválido")),
+            })
     @PutMapping("/{id}")
     public BaseDto<Void> updateAddress(@PathVariable String id, @RequestBody AddressUpdateDto addressUpdateDto) {
         List<BaseErrorDto> errors = new ArrayList<>();
-        try{
+        try {
             UUID idAddress = UUID.fromString(id);
             BaseDto updatedAddressId = updateAddressService.updateAddress(idAddress, addressUpdateDto);
             return updatedAddressId;
-        }catch (IllegalArgumentException e){
-            errors.add(new BaseErrorDto ("idAddress", ErrorMessage.INVALID_FIELD));
+        } catch (IllegalArgumentException e) {
+            errors.add(new BaseErrorDto("idAddress", ErrorMessage.INVALID_FIELD));
             ResponseErrorBuilder result = new ResponseErrorBuilder(HttpStatus.PRECONDITION_FAILED, errors);
             return result.get();
         }
